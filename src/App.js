b4aaa8b6285d7828a84d6123b9892fa78a0ab8ad
alexlucas <alex.plomberie83@gmail.com>
2019-04-18 21:39:33 +0200

    add navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import GameInit from './components/GameInit/GameInit';
 import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Navbar />
         <GameInit />  
         <Footer />     
       </div>
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Navbar.css';
+
+export default function Navbar() {
+  return (
+    <div>
+      <body>
+  <nav class="navbar">
+    <span class="navbar-toggle" id="js-navbar-toggle">
+            <i class="fas fa-bars"></i>
+        </span>
+    <a href="logo" className="logo">Easter Memory</a>
+  </nav>
+</body>
+    </div>
+  )
+}
