e895dd1348d9a7718d2b84ad0d64ecd91bab66ab
Dan CARBONE <dan.carbone@live.fr>
2019-04-18 22:36:53 +0200

    Modify app.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 //import GameInit from './components/GameInit/GameInit'
-const baseUrl = "easteregg.wildcodeschool.fr/api"
+
 
 class App extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
-      eggs: [],
+      eggs: { data: [{ id: 1, name: "Dan" }] },
       isLoading: false
     }
   }
 
   componentDidMount = () => {
     this.setState({ isLoading: true })
-
-    fetch(`${baseUrl}/eggs`)
-      .then(response => this.setState({ eggs: response.data.results }))
+    axios.get(`http://easteregg.wildcodeschool.fr/api/eggs`)
+      .then(data => this.setState({ eggs: data }))
       .catch(error => console.log(error))
   }
 
   render() {
-    console.log(this.state)
-    const { isLoading } = this.state;
-    if (isLoading) { return <p>Eggs Array Loading ...</p> }
+    console.log(this.state.eggs.data + "this is my eggs.data")
+    if (this.state.isLoading === true) { return <p>Eggs Array Loading ...</p> }
 
     return (
-      <div className="App">
-        {
-
-        }
-      </div>
+      this.state.eggs.data.map((egg, idx) => {
+        console.log('Line thirty one');
+        return (
+          <div key={`${idx}`}>
+            <h2>{egg.id}</h2>
+            <h2>{egg.name}</h2>
+          </div>
+        );
+      })
     )
   }
 }
-
 
 export default App;
