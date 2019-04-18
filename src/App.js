20434dffc3ef867a21cad081a89eb08b3226f79a
Dan CARBONE <dan.carbone@live.fr>
2019-04-19 00:09:29 +0200

    Modify App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
-//import GameInit from './components/GameInit/GameInit'
-
 
 class App extends Component {
 
