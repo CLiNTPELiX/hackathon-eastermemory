4c390a481f060ab0e1bf66341d3b2878555f8ce9
Dan CARBONE <dan.carbone@live.fr>
2019-04-18 18:12:50 +0200

    Modify app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
-import GameInit from './components/GameInit/GameInit'
+//import GameInit from './components/GameInit/GameInit'
+const baseUrl = "easteregg.wildcodeschool.fr/api"
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      eggs: [],
+      isLoading: false
+    }
+  }
+
+  componentDidMount = () => {
+    this.setState({ isLoading: true })
+
+    fetch(`${baseUrl}/eggs`)
+      .then(response => this.setState({ eggs: response.data.results }))
+      .catch(error => console.log(error))
+  }
+
   render() {
+    console.log(this.state)
+    const { isLoading } = this.state;
+    if (isLoading) { return <p>Eggs Array Loading ...</p> }
+
     return (
       <div className="App">
-        <GameInit />        
+        {
+
+        }
       </div>
-    );
+    )
   }
 }
+
 
 export default App;
