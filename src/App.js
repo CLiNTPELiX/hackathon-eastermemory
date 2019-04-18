9dc07bc5fb77049d397d1898a92d318a6a701606
Dan CARBONE <dan.carbone@live.fr>
2019-04-19 00:25:59 +0200

    Remove *footer virus*

--- src/App.js
+++ src/App.js
@@ -1,48 +1,16 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import './App.css';
-<<<<<<< HEAD
-=======
 import GameInit from './components/GameInit/GameInit';
 import GameMain from './components/GameMain/GameMain';
 import { Switch, Route } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
->>>>>>> 6e8f6f64858b56c317f0229c9a23eb08b7ecc20c
+
+import './App.css';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      eggs: { data: [{ id: 1, name: "Dan" }] },
-      isLoading: false
-    }
-  }
-
-  componentDidMount = () => {
-    this.setState({ isLoading: true })
-    axios.get(`http://easteregg.wildcodeschool.fr/api/eggs`)
-      .then(data => this.setState({ eggs: data }))
-      .catch(error => console.log(error))
-  }
-
   render() {
-    console.log(this.state.eggs.data + "this is my eggs.data")
-    if (this.state.isLoading === true) { return <p>Eggs Array Loading ...</p> }
-
+    
     return (
-<<<<<<< HEAD
-      this.state.eggs.data.map((egg, idx) => {
-        console.log('Line thirty one');
-        return (
-          <div key={`${idx}`}>
-            <h2>{egg.id}</h2>
-            <h2>{egg.name}</h2>
-          </div>
-        );
-      })
-    )
-=======
       <div className="App">
         <Switch>
           <Route path='/play' component={GameMain} />
@@ -51,7 +19,6 @@ class App extends Component {
         <Footer />
       </div>
     );
->>>>>>> 6e8f6f64858b56c317f0229c9a23eb08b7ecc20c
   }
 }
 
