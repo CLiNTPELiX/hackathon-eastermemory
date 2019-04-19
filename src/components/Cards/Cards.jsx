b3ca49f2548d69bcb0daad408405dc49645c4911
alexlucas <alex.plomberie83@gmail.com>
2019-04-19 02:19:30 +0200

    first commit cards

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,381 @@
+let eggs = [
+  {
+    'id': '5cac51240d488f0da6151bcd',
+    'name': 'Egg of Tuna',
+    'color': '#7e3703',
+    'caliber': 'L',
+    'farming': 3,
+    'country': 'KP',
+    'rarity': 'junk',
+    'image': 'https://cdn.shopify.com/s/files/1/0993/5182/products/CXBO_Chocolate_Disco_Egg_large.png?v=1519913456',
+    'power': 'decrease physical proctection:9',
+    'meta': {
+      'revision': 0,
+      'created': 1554982481291,
+      'version': 0
+    },
+    '$loki': 1,
+    'validity': '2019-04-19T02:24:19.450Z'
+  },
+  {
+    'id': '5cac51240d488f0da6151bce',
+    'name': 'Egg of Flounder Winter',
+    'color': '#0a567f',
+    'caliber': 'M',
+    'farming': 1,
+    'country': 'SN',
+    'rarity': 'legendary',
+    'image': 'https://shop.whitehousehistory.org/media/catalog/product/t/r/trump_egg_gold_shop.png',
+    'power': 'decrease physical proctection:5',
+    'meta': {
+      'revision': 0,
+      'created': 1554982481291,
+      'version': 0
+    },
+    '$loki': 2,
+    'validity': '2019-04-19T04:32:19.450Z'
+  },
+  {
+    'id': '5cac51240d488f0da6151bcf',
+    'name': 'Egg of Stickleback three spine',
+    'color': '#46313d',
+    'caliber': 'M',
+    'farming': 3,
+    'country': 'NL',
+    'rarity': 'fine',
+    'image': 'http://pixelartmaker.com/art/2480187d8bd9102.png',
+    'power': 'decrease physical power:3',
+    'meta': {
+      'revision': 0,
+      'created': 1554982481291,
+      'version': 0
+    },
+    '$loki': 3,
+    'validity': '2019-04-19T02:38:19.450Z'
+  },
+  {
+    'id': '5cac51240d488f0da6151bd0',
+    'name': 'Egg of Flounder Winter',
+    'color': '#7d2c7f',
+    'caliber': 'M',
+    'farming': 3,
+    'country': 'MF',
+    'rarity': 'junk',
+    'image': 'https://www.buttermilk.co.uk/wp-content/uploads/2018/02/css-egg-web.png',
+    'power': 'decrease magical proctection:2',
+    'meta': {
+      'revision': 0,
+      'created': 1554982481291,
+      'version': 0
+    },
+    '$loki': 4,
+    'validity': '2019-04-19T02:10:19.450Z'
+  },
+  {
+    'id': '5cac51240d488f0da6151bd1',
+    'name': 'Egg of Cobra Indian',
+    'color': '#20253c',
+    'caliber': 'M',
+    'farming': 0,
+    'country': 'HR',
+    'rarity': 'exotic',
+    'image': 'http://pixelartmaker.com/art/2480187d8bd9102.png',
+    'power': 'decrease physical power:8',
+    'meta': {
+      'revision': 0,
+      'created': 1554982481291,
+      'version': 0
+    },
+    '$loki': 5,
+    'validity': '2019-04-19T00:29:19.450Z'
+  },
+  {
+    'id': '5cac51240d488f0da6151bd2',
+    'name': 'Egg of Fighting Fish Siamese',
+    'color': '#0a667d',
+    'caliber': 'XL',
+    'farming': 3,
+    'country': 'NP',
+    'rarity': 'basic',
+    'image': 'https://vignette.wikia.nocookie.net/bubble-gum-simulator/images/7/72/Lucky_Egg.png/revision/latest?cb=20190315192058',
+    'power': 'decrease physical proctection:4',
+    'meta': {
+      'revision': 0,
+      'created': 1554982481291,
+      'version': 0
+    },
+    '$loki': 6,
+    'validity': '2019-04-19T02:18:19.450Z'
+  },
+  {
+    'id': '5cac51240d488f0da6151bd3',
+    'name': 'Egg of Fighting Fish Siamese',
+    'color': '#582c33',
+    'caliber': 'XL',
+    'farming': 3,
+    'country': 'IO',
+    'rarity': 'ascended',
+    'image': 'https://shop.whitehousehistory.org/media/catalog/product/t/r/trump_egg_gold_shop.png',
+    'power': 'decrease movement speed:3',
+    'meta': {
+      'revision': 0,
+      'created': 1554982481291,
+      'version': 0
+    },
+    '$loki': 7,
+    'validity': '2019-04-19T00:40:19.450Z'
+  },
+  {
+    'id': '5cac51240d488f0da6151bd4',
+    'name': 'Egg of Stickleback three spine',
+    'color': '#7f411c',
+    'caliber': '3XL',
+    'farming': 3,
+    'country': 'MD',
+    'rarity': 'fine',
+    'image': 'https://www.buttermilk.co.uk/wp-content/uploads/2018/02/css-egg-web.png',
+    'power': 'increase physical proctection:4',
+    'meta': {
+      'revision': 0,
+      'created': 1554982481291,
+      'version': 0
+    },
+    '$loki': 8,
+    'validity': '2019-04-19T02:57:19.450Z'
+  },
+  {
+    'id': '5cac51240d488f0da6151bd5',
+    'name': 'Egg of Kiwi',
+    'color': '#5f4622',
+    'caliber': 'XS',
+    'farming': 3,
+    'country': 'MS',
+    'rarity': 'junk',
+    'image': 'https://www.sideshow.com/storage/product-images/903635/velociraptor-egg_jurassic-park_silo.png',
+    'power': 'increase attack speed:1',
+    'meta': {
+      'revision': 0,
+      'created': 1554982481291,
+      'version': 0
+    },
+    '$loki': 9,
+    'validity': '2019-04-19T00:56:19.450Z'
+  },
+  {
+    'id': '5cac51240d488f0da6151bd6',
+    'name': 'Egg of Magpie Black-billed',
+    'color': '#24003a',
+    'caliber': 'S',
+    'farming': 0,
+    'country': 'KH',
+    'rarity': 'legendary',
+    'image': 'https://i1.wp.com/borgattis.com/wp-content/uploads/2019/03/blue-egg.png?fit=861%2C877&ssl=1',
+    'power': 'decrease magical proctection:3',
+    'meta': {
+      'revision': 0,
+      'created': 1554982481291,
+      'version': 0
+    },
+    '$loki': 10,
+    'validity': '2019-04-19T04:36:19.450Z'
+  },
+  {
+    'id': '5cac51240d488f0da6151bd7',
+    'name': 'Egg of Salmon Chum',
+    'color': '#4f3544',
+    'caliber': '2XL',
+    'farming': 1,
+    'country': 'AM',
+    'rarity': 'legendary',
+    'image': 'https://shop.whitehousehistory.org/media/catalog/product/t/r/trump_egg_gold_shop.png',
+    'power': 'increase physical power:7',
+    'meta': {
+      'revision': 0,
+      'created': 1554982481291,
+      'version': 0
+    },
+    '$loki': 11,
+    'validity': '2019-04-19T00:44:19.450Z'
+  },
+  {
+    'id': '5cac51240d488f0da6151bd8',
+    'name': 'Egg of Chameleon',
+    'color': '#2b6d2a',
+    'caliber': '2XL',
+    'farming': 3,
+    'country': 'FI',
+    'rarity': 'exotic',
+    'image': 'http://pixelartmaker.com/art/2480187d8bd9102.png',
+    'power': 'decrease physical power:6',
+    'meta': {
+      'revision': 0,
+      'created': 1554982481291,
+      'version': 0
+    },
+    '$loki': 12,
+    'validity': '2019-04-19T03:09:19.450Z'
+  },
+  {
+    'id': '5cac51240d488f0da6151bd9',
+    'name': 'Egg of Salmon Chum',
+    'color': '#1b7768',
+    'caliber': '3XL',
+    'farming': 1,
+    'country': 'PM',
+    'rarity': 'legendary',
+    'image': 'https://www.sideshow.com/storage/product-images/903635/velociraptor-egg_jurassic-park_silo.png',
+    'power': 'decrease attack speed:8',
+    'meta': {
+      'revision': 0,
+      'created': 1554982481291,
+      'version': 0
+    },
+    '$loki': 13,
+    'validity': '2019-04-19T01:00:19.450Z'
+  },
+  {
+    'id': '5cac51240d488f0da6151bda',
+    'name': 'Egg of Flounder Winter',
+    'color': '#373d16',
+    'caliber': 'L',
+    'farming': 2,
+    'country': 'MT',
+    'rarity': 'ascended',
+    'image': 'http://pixelartmaker.com/art/2480187d8bd9102.png',
+    'power': 'decrease physical proctection:4',
+    'meta': {
+      'revision': 0,
+      'created': 1554982481291,
+      'version': 0
+    },
+    '$loki': 14,
+    'validity': '2019-04-19T03:09:19.450Z'
+  },
+  {
+    'id': '5cac51240d488f0da6151bdb',
+    'name': 'Egg of Pumpkinseed',
+    'color': '#331f0b',
+    'caliber': 'S',
+    'farming': 2,
+    'country': 'BM',
+    'rarity': 'ascended',
+    'image': 'https://i1.wp.com/borgattis.com/wp-content/uploads/2019/03/blue-egg.png?fit=861%2C877&ssl=1',
+    'power': 'decrease magical power:2',
+    'meta': {
+      'revision': 0,
+      'created': 1554982481291,
+      'version': 0
+    },
+    '$loki': 15,
+    'validity': '2019-04-19T03:07:19.450Z'
+  },
+  {
+    'id': '5cac51240d488f0da6151bdc',
+    'name': 'Egg of Tuna',
+    'color': '#437917',
+    'caliber': '3XL',
+    'farming': 1,
+    'country': 'KZ',
+    'rarity': 'exotic',
+    'image': 'https://i1.wp.com/borgattis.com/wp-content/uploads/2019/03/blue-egg.png?fit=861%2C877&ssl=1',
+    'power': 'decrease movement speed:7',
+    'meta': {
+      'revision': 0,
+      'created': 1554982481291,
+      'version': 0
+    },
+    '$loki': 16,
+    'validity': '2019-04-19T02:30:19.450Z'
+  }
+];
+const gameGrid = cards.concat(cards);
+//shuffle array of cards
+gameGrid.sort(() => 0.5 - Math.random());
+    
+// global vars
+let count = 0;
+let firstGuess = '';
+let secondGuess = '';
+let previousTarget = null;
+let delay = 1200;
+    
+const grid = document.getElementById('grid');
+    
+// card creation loop
+gameGrid.forEach(item => {
+  const card = document.createElement('div');
+  card.classList.add('card');
+  card.dataset.name = item.name;
+    
+  // create front of card
+  const front = document.createElement('div');
+  front.classList.add('front');
+    
+  // create back of card
+  const back = document.createElement('div');
+  back.classList.add('back');
+  back.style.backgroundImage = `url(${item.img})`;
+    
+  // append card to grid and front and back to each card
+  grid.appendChild(card);
+  card.appendChild(front);
+  card.appendChild(back);
+});
+    
+const match = () => {
+  const selected = document.querySelectorAll('.selected');
+  console.log(selected);
+    
+  selected.forEach(card => {
+    card.classList.add('match');
+  });
+};
+    
+const resetGuesses = () => {
+  firstGuess = '';
+  secondGuess = '';
+  count = 0;
+  previousTarget = null;
+    
+  let selected = document.querySelectorAll('.selected');
+  selected.forEach(card => {
+    card.classList.remove('selected');
+  });
+};
+    
+grid.addEventListener('click', event => {
+  let clicked = event.target;
+  // console.log(event);
+    
+  if (
+    clicked.nodeName === 'SECTION' ||
+        clicked === previousTarget ||
+        clicked.parentNode.classList.contains('selected') ||
+        clicked.parentNode.classList.contains('match')
+  ) {
+    return;
+  }
+    
+  //add selected class
+  if (count < 2) {
+    count++;
+    if (count === 1) {
+      // Assign first guess
+      firstGuess = clicked.parentNode.dataset.name;
+      clicked.parentNode.classList.add('selected');
+    } else {
+      secondGuess = clicked.parentNode.dataset.name;
+      clicked.parentNode.classList.add('selected');
+    }
+    
+    // create the delay
+    if (firstGuess && secondGuess) {
+      if (firstGuess === secondGuess) {
+        setTimeout(match, delay);
+      }
+      setTimeout(resetGuesses, delay);
+    }
+    previousTarget = clicked;
+  }
+});
+    
