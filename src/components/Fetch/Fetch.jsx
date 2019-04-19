cf59e41844213dbe27b8aa7c9c72bcd6d5725930
Barenziah <lunasecrete@gmail.com>
2019-04-19 08:55:05 +0200

    Add method on Fetch component

--- src/components/Fetch/Fetch.jsx
+++ src/components/Fetch/Fetch.jsx
@@ -7,14 +7,39 @@ export default class Fetch extends Component {
     super(props);
     this.state = {
       eggs: { data: [{ id: 1337, name: 'Dan', image: 'http://myimage' }] },
-      loading: true
+      loading: true,
+      eggsIWant:[],
+      eggsIReallyWant:[],
+      isFiltered: false,
+      difficultyLevel: 3
     };
+    
+  }
+
+  getArrayOfX(){
+    let cardsNumber = 0;
+    if (this.state.difficultyLevel === 1) {
+      cardsNumber = 8;
+    } else if (this.state.difficultyLevel === 2) {
+      cardsNumber = 16;
+    } else if (this.state.difficultyLevel === 3) {
+      cardsNumber = 32;
+    }
+
+    for (let i = 0; i < cardsNumber; i++){
+      if (this.state.isFiltered === false) {
+        this.state.eggsIWant.push(this.state.eggs.data[Math.floor(Math.random() * this.state.eggs.data.length)].image);
+      }
+    }
+    this.setState({eggsIReallyWant: this.state.eggsIWant, isFiltered : true});
+    console.log('I run l15 func');
   }
 
   componentDidMount = () => {
     axios.get('http://easteregg.wildcodeschool.fr/api/eggs')
       .then(data => this.setState({ eggs: data }))
       .catch(error => console.log(error));
+    this.getArrayOfX(2);
   }
 
   render() {
@@ -26,13 +51,10 @@ export default class Fetch extends Component {
     }
 
     return (
-      this.state.eggs.data.map((egg, idx) => {
-        return (
-          <div key={`${idx}`}>
-            ${idx}
-            
-          </div>
-        );
+      console.log(this.state.eggsIWant + ' want'),
+      console.log(this.state.eggsIReallyWant + ' really want'),
+      this.state.eggs.data.map((egg) => {
+        return <img src={egg.image} alt='noAlternative'></img>;
       })
     );
   }
